5c47f6a2184381ee188b2b391a7bdf0f51c0b894
Julio Liu <kennkiunn@gmail.com>
2023-06-17 16:08:24 -0500

    AcademloMotors

--- src/routes/repairs.routes.js
+++ src/routes/repairs.routes.js
@@ -6,11 +6,11 @@ const { validRepairId } = require('../middlewares/repairs.middleware')
 
 
 
-router.get('/', restricTo('client'), findRepairs)
+router.get('/', restricTo('employee'), findRepairs)
 router.post('/', createRepair)
 
-router.get('/:id', restricTo('client'), validRepairId, findARepair)
-router.patch('/:id', restricTo('client'), validRepairId, updateRepair)
-router.delete('/:id', restricTo('client'), validRepairId, deleteRepair)
+router.get('/:id', restricTo('employee'), validRepairId, findARepair)
+router.patch('/:id', restricTo('employee'), validRepairId, updateRepair)
+router.delete('/:id', restricTo('employee'), validRepairId, deleteRepair)
 
 module.exports = router
